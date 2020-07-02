cfc41c60bc020568c1271dc3c60b8428a3009e80
Alessandro Portes <alessandro.portes@gmail.com>
2020-07-02 04:12:12 -0300

    Respondendo texto especifico

---
+++ exercicios/hears.js
@@ -0,0 +1,16 @@
+const env = require("../.env");
+const Telegraf = require("telegraf");
+const moment = require("moment");
+const bot = new Telegraf(env.token);
+
+bot.hears("pizza", (ctx) => ctx.reply("Quero!"));
+bot.hears(["figado", "chuchu"], (ctx) => ctx.reply("Passo!"));
+bot.hears(/burguer/i, (ctx) => ctx.reply("Quero!")); // Passando uma expressão regular
+bot.hears([/burguer/i, /salada/i], (ctx) => ctx.reply("Quero!")); // Passando uma expressão regular
+bot.hears(/(\d{2}\/\d{2}\/\d{4})/, (ctx) => {
+  moment.locale("pt-BR");
+  const data = moment(ctx.match[1], "DD/MM/YYYY");
+  ctx.reply(`${ctx.match[1]} cai em ${data.format("dddd")}`);
+});
+
+bot.startPolling();
